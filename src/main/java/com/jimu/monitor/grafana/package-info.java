12880048a9dc9ef9704790c32b6f52f033f91633
longqiang.cao <longqiang.cao@alphayee.com>
2018-01-16 14:26:48 +0800

    add

--- src/main/java/com/jimu/monitor/grafana/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * Created by zhenbao.zhou on 16/6/4.
- * 存放grafana相关的操作
- */
-package com.jimu.monitor.grafana;
--- src/main/java/com/jimu/monitor/utils/http/package-info.java
+++
@@ -1,1 +0,0 @@
-/** * httpclient 封装 * Created by jimin on 16/3/10. */package com.jimu.monitor.utils.http;
